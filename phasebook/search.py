c2c834f0e7d55828477ae35bf794dc47b51ab04d
Roy Christian Pery <thercpery@gmail.com>
2024-05-28 14:35:19 +0800

    ✨ feat: added searching of users based on parameters

--- phasebook/search.py
+++ phasebook/search.py
@@ -1,5 +1,5 @@
+import re
 from flask import Blueprint, request
-
 from .data.search_data import USERS
 
 
@@ -8,6 +8,7 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 
 @bp.route("")
 def search():
+    print(request.args.to_dict())
     return search_users(request.args.to_dict()), 200
 
 
@@ -24,7 +25,16 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
+    
+    results = []
+    for user in USERS:
+        for key, value in args.items():
+            if type(user[key]) == str:
+                regexFormat = f"({value.lower()})"
+                result = re.findall(regexFormat, user[key].lower())
+                if len(result) > 0:
+                    results.append(user)
+            if type(user[key]) == int and user[key] == int(value):
+                results.append(user)
 
-    # Implement search here!
-
-    return USERS
+    return results
